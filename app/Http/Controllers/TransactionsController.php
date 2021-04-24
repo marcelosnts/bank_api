77d691e45a8b497bcbbaca9e369ed9d0875be191
Marcelo <santosmarcelo1995@live.com>
2021-04-24 15:17:29 -0300

    Add TransactionsController scaffold

---
+++ app/Http/Controllers/TransactionsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransactionsController extends Controller
+{
+    public function history()
+    {
+        return;
+    }
+
+    public function deposit()
+    {
+        return;
+    }
+
+    public function withdraw()
+    {
+        return;
+    }
+}
