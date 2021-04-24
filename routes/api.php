ea7dc613bd40992dce30570db9e1d954099d7391
Marcelo <santosmarcelo1995@live.com>
2021-04-24 14:59:26 -0300

    Change route user to me

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::group(['prefix' => 'auth'], function () {
 
     Route::group(['middleware' => 'auth:api'], function() {
         Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
+        Route::get('me', 'AuthController@user');
     });
 });
 
