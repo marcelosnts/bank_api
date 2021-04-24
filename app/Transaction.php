0fa7a382554c7a3a63f7ed8dfdec55fac534606d
Marcelo <santosmarcelo1995@live.com>
2021-04-24 16:44:09 -0300

    Add transaction consts / get text function

--- app/Transaction.php
+++ app/Transaction.php
@@ -9,4 +9,16 @@ class Transaction extends Model
     protected $fillable = [
         'type', 'value', 'user_id'
     ];
+
+    public const DEPOSIT = 1;
+    public const WITHDRAW = 2;
+
+    public static function getTypeText($type)
+    {
+        $labels = [
+            1 => 'Deposit',
+            2 => 'Withdraw'
+        ];
+        return $labels[$type];
+    }
 }
