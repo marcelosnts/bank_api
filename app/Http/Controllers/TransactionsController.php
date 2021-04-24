d2feb755dfcf29789213fd430f4dbb89eb7de710
Marcelo <santosmarcelo1995@live.com>
2021-04-24 16:00:49 -0300

    Add TransactionController new method

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -4,20 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Transaction;
+
 class TransactionsController extends Controller
 {
+    private $type = [
+        1 => 'Deposit',
+        2 => 'Withdraw'
+    ];
+
     public function history()
     {
         return;
     }
 
-    public function deposit()
+    public function new(Request $request)
     {
-        return;
-    }
+        $rules = [
+            'type' => 'required|min:1|max:2|integer',
+            'value' => 'required|min:0|numeric'
+        ];
 
-    public function withdraw()
-    {
-        return;
+        $request->validate($rules);
+
+        $transaction = new Transaction([
+            'type' => $request->type,
+            'value' => $request->value,
+            'user_id' => $request->user()->id
+        ]);
+
+        $transaction->save();
+
+        return response()->json([
+            'message' => "{$this->type[$request->type]} submited!"
+        ], 201);
     }
 }
